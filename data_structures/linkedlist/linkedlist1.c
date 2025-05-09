d651b6cbe5895887f51bc3151ef8f3870c849aff
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-05-09 22:35:31 +0330

    linkedListInit add prepend function

---
+++ data_structures/linkedlist/linkedlist1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    struct Node* next;
+    int data;
+}Node;
+
+typedef struct LinkedList
+{
+    Node *head;
+}LinkedList;
+
+Node* createNode(int data);
+LinkedList* listInit(LinkedList* l);
+void prepend(LinkedList *l, int data);
+void printll(LinkedList *l);
+
+int main(void)
+{
+    LinkedList* l1 = listInit(l1);
+    prepend(l1, 12);
+    prepend(l1, 102);
+    prepend(l1, 120);
+    prepend(l1, 122);
+
+    printll(l1);
+
+
+    return 0;
+}
+
+LinkedList* listInit(LinkedList* l)
+{
+    l = (LinkedList*) malloc(sizeof(LinkedList));
+    l->head = NULL;
+    return l;
+}
+
+Node* createNode(int data)
+{
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+
+void prepend(LinkedList *l, int data)
+{
+    Node *node = createNode(data);
+    if(l->head == NULL)
+    {
+        l->head = node;
+        return;
+    }
+
+    node->next = l->head;
+    l->head = node;
+}
+
+void insertAt(LinkedList *l)
+{
+
+}
+
+void append(LinkedList *l)
+{
+    
+}
+
+void printll(LinkedList *l)
+{
+    Node* cur = l->head;
+    while(cur != NULL)
+    {
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+    printf("\n");
+}
